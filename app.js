63ae284e66cba4f15a27cf1af95812dcec15b2fc
NotLucas777 <luquinhasgpdm090@gmail.com>
2024-07-24 16:21:39 -0300

    header development

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Ola")
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeButton = document.getElementById('darkModeButton');
+    const userButton = document.getElementById('userButton');
+    const body = document.body;
+    const header = document.querySelector('header');
+
+    darkModeButton.addEventListener('click', () => {
+        document.documentElement.classList.toggle('dark-mode')
+    });
+
+    userButton.addEventListener('click', () => {
+        alert('Ação do Usuário');
+    });
+});
